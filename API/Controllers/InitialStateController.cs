7a82eee951ea70c0f98b3036694e9e460697725b
Snir Agi <sniragi123@gmail.com>
2023-09-11 16:41:35 +0300

    fix the initial state that all the object return without the users and create a react project

--- API/Controllers/InitialStateController.cs
+++ API/Controllers/InitialStateController.cs
@@ -8,9 +8,9 @@ namespace API.Controllers
 	[Route("api/[controller]")]
 	public class InitialStateController : Controller
 	{
-		//private readonly IReviewRepository _reviewRepo;
+		private readonly IReviewRepository _reviewRepo;
 		//private readonly IUserRepository _userRepo;
-		//private readonly IOrderItemRepository _orderItemRepo;
+		private readonly IOrderItemRepository _orderItemRepo;
 		private readonly IConfiguration _config;
 		private readonly IProductRepository _productRepo;
 		private readonly IOrderRepository _orderRepo;
@@ -20,16 +20,16 @@ namespace API.Controllers
 			IConfiguration _config,
 			ICategoryRepository _categoryRepo,
 			IOrderRepository _orderRepo,
-			IProductRepository _productRepo
-			//IOrderItemRepository _orderItemRepo,
+			IProductRepository _productRepo,
+			IOrderItemRepository _orderItemRepo,
 			//IUserRepository _userRepo,
-			//IReviewRepository _reviewRepo
+			IReviewRepository _reviewRepo
 			)
 		{
 			this._config = _config ?? throw new ArgumentNullException(nameof(_config));
-			//this._reviewRepo = _reviewRepo ?? throw new ArgumentNullException(nameof(_reviewRepo));
+			this._reviewRepo = _reviewRepo ?? throw new ArgumentNullException(nameof(_reviewRepo));
 			//this._userRepo = _userRepo ?? throw new ArgumentNullException(nameof(_userRepo));
-			//this._orderItemRepo = _orderItemRepo ?? throw new ArgumentNullException(nameof(_orderItemRepo));
+			this._orderItemRepo = _orderItemRepo ?? throw new ArgumentNullException(nameof(_orderItemRepo));
 			this._productRepo = _productRepo ?? throw new ArgumentNullException(nameof(_productRepo));
 			this._orderRepo = _orderRepo ?? throw new ArgumentNullException(nameof(_orderRepo));
 			this._categoryRepo = _categoryRepo ?? throw new ArgumentNullException(nameof(_categoryRepo));
@@ -41,10 +41,10 @@ namespace API.Controllers
 			InitialStateData result = InitialStateData.Create(
 				_categoryRepo,
 				_orderRepo,
-				_productRepo
-				//_orderItemRepo,
+				_productRepo,
+				_orderItemRepo,
 				//_userRepo,
-				//_reviewRepo
+				_reviewRepo
 				);
 
 			return Ok(result);
--- API/Models/InitialStateData.cs
+++ API/Models/InitialStateData.cs
@@ -9,9 +9,9 @@ namespace API.Models
 		{
 		}
 
-		//public List<Review> Reviews { get; private set; }
+		public List<Review> Reviews { get; private set; }
 		//public List<User> Users { get; private set; }		
-		//public List<OrderItem> OrderItems { get; private set; }
+		public List<OrderItem> OrderItems { get; private set; }
 		public List<Product> Products { get; private set; }
 		public List<Order> Orders { get; private set; }
 		public List<Category> Categories { get; private set; }
@@ -19,17 +19,17 @@ namespace API.Models
 		internal static InitialStateData Create(
 			ICategoryRepository _categoryRepo,
 			IOrderRepository _orderRepo,
-			IProductRepository _productRepo
-			//IOrderItemRepository _orderItemRepo,
+			IProductRepository _productRepo,
+			IOrderItemRepository _orderItemRepo,
 			//IUserRepository _userRepo,
-			//IReviewRepository _reviewRepo
+			IReviewRepository _reviewRepo
 			)
 		{
 			InitialStateData initialStateData = new InitialStateData();
 
-			//initialStateData.Reviews = _reviewRepo.FindAll().ToList();
+			initialStateData.Reviews = _reviewRepo.FindAll().ToList();
 			//initialStateData.Users = _userRepo.FindAll().ToList();
-			//initialStateData.OrderItems = _orderItemRepo.FindAll().ToList();
+			initialStateData.OrderItems = _orderItemRepo.FindAll().ToList();
 			initialStateData.Products = _productRepo.FindAll().ToList();
 			initialStateData.Orders = _orderRepo.FindAll().ToList();
 			initialStateData.Categories = _categoryRepo.FindAll().ToList();
